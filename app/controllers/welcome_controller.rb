4ae6ff0ec4254a54da6c3a4e636186779fb27b76
pavel <pavel@ubuntu.(none)>
2014-09-13 16:44:19 +0400

    Init

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,9 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def show_all_emails
+  	
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ClientEmail::Application.config.secret_key_base = 'b51b88474cf557638e0d1ba23a984cb9e51cc0bc237a71c1545363c570749fbab7349594a7b38a0f8d25450987eb115ad46bd2b6fcfebfac5d9557c34b6cb998'
